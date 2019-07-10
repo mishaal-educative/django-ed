e545fbfc4e1ffe03fb20807621ace778c640e181
mishaal-educative <51785160+mishaal-educative@users.noreply.github.com>
2019-07-10 17:16:24 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-PYTHONPATH=`pwd`/.. gunicorn --bind 0.0.0.0:3000 helloworld.wsgi:application
+PYTHONPATH=`pwd`/.. gunicorn --bind 0.0.0.0:8000 helloworld.wsgi:application
 
 
 
